48fd2f9597e1ed270e7c45c0f17b898c8978613e
Harsh Makani <harshvardhanmakani@gmail.com>
2019-01-23 21:35:46 -0600

    Updates

---
+++ precache-manifest.fe3cc90cf5bfb6ed1ef367ef76a3a0fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b33adab4536a7d986207",
+    "url": "/static/js/main.b33adab4.chunk.js"
+  },
+  {
+    "revision": "b3a13d4458ce9f8c58e6",
+    "url": "/static/js/1.b3a13d44.chunk.js"
+  },
+  {
+    "revision": "b33adab4536a7d986207",
+    "url": "/static/css/main.22f53f70.chunk.css"
+  },
+  {
+    "revision": "0ae343ae48a6c25b63383ec4875f29f3",
+    "url": "/index.html"
+  }
+];
